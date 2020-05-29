ba281823d675f0afd0b815401ecaa4de2f7e005b
dom <crhicko@protonmail.com>
2020-05-29 12:30:43 -0400

    Added new unity input system, got basic exmaple working
    Press q to generate a log message
    If this isnt working make sure to revert Unity VSCODE support in package manager to version 1.1.2

---
+++ Assets/Resources/Scripts/MoveController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoveController : MonoBehaviour
+{
+
+    void Awake() {
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RotateLeft(InputAction.CallbackContext context) {
+        Debug.Log("rotating left");
+    }
+}

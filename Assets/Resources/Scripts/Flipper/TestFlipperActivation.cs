55e54e1199aa85d807e0e398bdccd316fb14fff5
dom <crhicko@protonmail.com>
2020-06-01 21:28:59 -0400

    Got a flipper activation working

---
+++ Assets/Resources/Scripts/Flipper/TestFlipperActivation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFlipperActivation : MonoBehaviour, IActivatable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Activate() {
+        Debug.Log("Activated this lad");
+    }
+}

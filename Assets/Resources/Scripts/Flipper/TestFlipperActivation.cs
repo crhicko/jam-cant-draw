c59a76b1bf6c12601d21fd1c41b38654da82e48f
dom <crhicko@protonmail.com>
2020-06-02 13:23:01 -0400

    trying to get some animations to work

--- Assets/Resources/Scripts/Flipper/TestFlipperActivation.cs
+++ Assets/Resources/Scripts/Flipper/TestFlipperActivation.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TestFlipperActivation : MonoBehaviour, IActivatable
 {
+    public Animator animator;
+    public bool triggerAnimation = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +20,7 @@ public class TestFlipperActivation : MonoBehaviour, IActivatable
 
     public void Activate() {
         Debug.Log("Activated this lad");
+        gameObject.SetActive(true);
+        animator.SetTrigger("isShot");
     }
 }

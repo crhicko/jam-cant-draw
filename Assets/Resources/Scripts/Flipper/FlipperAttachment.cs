c69fb9353fc9298b40f6da3b544e6d894f247303
dom <crhicko@protonmail.com>
2020-06-01 21:35:44 -0400

    Update player prefab and add FlipperAttachment prefab

--- Assets/Resources/Scripts/Flipper/FlipperAttachment.cs
+++ Assets/Resources/Scripts/Flipper/FlipperAttachment.cs
@@ -5,12 +5,12 @@ using UnityEngine.InputSystem;
 
 public class FlipperAttachment : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject flipper;
+    public GameObject flipper;
     // Start is called before the first frame update
     void Start()
     {
-
+        if(flipper != null)
+            Instantiate(flipper, new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame

39e0ddeed6a111673cd635aba794461c290381d5
theNumberSeven <thenumbersevengames@gmail.com>
2020-06-01 21:11:56 -0400

    Rink work
    Puck functional, goals functional, middle line functional.

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject eastGoal;
+    public GameObject puck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreCollision(eastGoal.GetComponent<BoxCollider2D>(), puck.GetComponent<CircleCollider2D>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Resources/Scripts/Goal.cs
@@ -0,0 +1,30 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public GameObject puck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreCollision(gameObject.GetComponent<BoxCollider2D>(), puck.GetComponent<CircleCollider2D>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+     void OnCollisionEnter2D(Collision2D collision)
+     {
+         if(collision.gameObject == puck)
+         {
+             Debug.Log("HIT");
+             puck.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+         }
+     }
+
+}
